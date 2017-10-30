7623867629f4fb74613171f55bd0b950c891bba9
Ash <quarktheawesome@gmail.com>
2017-10-30 19:23:05 +1100

    Add libjpeg-turbo module

--- compile.sh
+++ compile.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+if [ -f ext/lib/libjpeg.a ]
+then
+    echo "libjpeg already built; skipping..."
+else
+    cd ext/src/libjpeg-turbo
+    autoreconf --install
+    ./configure --host=powerpc-eabi --prefix=$(dirname $(dirname $(pwd))) --disable-shared --without-simd CFLAGS=-mwup
+    make $@
+    make install
+    cd ../../..
+fi
 
 mkdir build
 cd build
